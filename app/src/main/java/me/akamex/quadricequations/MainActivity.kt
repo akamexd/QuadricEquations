26ab41926601d853245a8f5f415deea9779585ef
nikita <97367701+akamexd@users.noreply.github.com>
2022-03-13 23:11:27 +0300

    little fixes

--- app/src/main/java/me/akamex/quadricequations/MainActivity.kt
+++ app/src/main/java/me/akamex/quadricequations/MainActivity.kt
@@ -1,17 +1,20 @@
 package me.akamex.quadricequations
 
 import android.annotation.SuppressLint
+import android.app.ActionBar
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Html
+import android.text.Layout
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import me.akamex.quadricequations.equation.*
 
 class MainActivity : AppCompatActivity() {
@@ -74,14 +77,24 @@ class MainActivity : AppCompatActivity() {
                         return@apply
                     }
 
-                    findViewById<TextView>(R.id.result).apply {
-                        visibility = if(result.result!!.isNotEmpty()) View.VISIBLE else View.INVISIBLE
-                        text = Html.fromHtml(result.result, Html.FROM_HTML_MODE_LEGACY)
+                    val steps = result.steps
+                    val resultValue = result.result
+
+                    var textViewOld: TextView? = null
+                    resultValue?.split("\n")?.forEach {
+                        val textView = TextView(this@MainActivity)
+
+                        val layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                        layoutParams.addRule(RelativeLayout.ALIGN_START, R.id.getResult)
+                        layoutParams.addRule(RelativeLayout.BELOW, if(textViewOld == null) R.id.getResult else textViewOld!!.id)
+                        textView.layoutParams = layoutParams
+                        textView.text = Html.fromHtml(it, Html.FROM_HTML_MODE_LEGACY)
+
+                        findViewById<RelativeLayout>(R.id.resultBlock).addView(textView)
+
+                        textViewOld = textView
                     }
-                    findViewById<TextView>(R.id.steps).apply {
-                        visibility = if(result.steps.isNotEmpty()) View.VISIBLE else View.INVISIBLE
-                        text = Html.fromHtml(result.steps.joinToString("\n"), Html.FROM_HTML_MODE_LEGACY)
-                    }
+
                 }
             }
         }
